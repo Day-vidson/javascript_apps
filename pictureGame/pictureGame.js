4107b649989a77940961a27f2fe910240c76835e
Dawid <dawifd54@gmail.com>
2020-03-13 16:40:28 +0100

    dawid

--- pictureGame/pictureGame.js
+++ pictureGame/pictureGame.js
@@ -4,6 +4,7 @@ function init() {
     var images = document.getElementsByTagName("img")
     for(var i = 0; i < images.length; i++) {
         images[i].onmouseover = showAnswer
+        images[i].onmouseout = reblur
     }
 }
 
@@ -12,11 +13,12 @@ function showAnswer(eventObj) {
     var name = image.id;
     name = name + ".jpg"
     image.src = name
-    
+
     // setTimeout(reblur, 2000, image)
 }
 
-function reblur(image) {
+function reblur(eventObj) {
+    var image = eventObj.target
     var name = image.id
     name = name + "blur.jpg"
     image.src = name
