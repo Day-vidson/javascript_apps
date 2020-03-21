8624b0e6474b733ff3336a6184dfca4fe11ae848
dawidLaptop <dawifd54@gmail.com>
2020-03-21 17:55:35 +0100

    winOrLose

---
+++ winOrLose.js
@@ -0,0 +1,37 @@
+var winner = function() {console.log("Farciarz!")}
+var loser = function() {console.log("Leszczu!")}
+winner()
+var a = winner
+var b = loser
+var c = loser
+a()
+b()
+c = a
+console.log(a)  //winner
+console.log(c)  //winner
+
+a = b
+console.log(a)  //loser
+console.log(b)  //loser
+
+b = c
+console.log(b)  //winner
+console.log(c)  //winner
+
+c = a
+console.log(a)  //loser
+console.log(c)  //loser
+
+a = c
+console.log(a)  //loser
+console.log(c)  //loser
+
+a = b
+console.log(a)  //winner
+console.log(b)  //winner
+
+b = c
+console.log(b)   //loser
+console.log(c)   //loser
+
+a()
