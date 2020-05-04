924a2db3cd0798ca1cd505e52259a4c4c9ec5108
Dawid <dawifd54@gmail.com>
2020-05-04 18:27:41 +0200

    questions

--- questions/1.js
+++ questions/1.js
@@ -1,17 +1,19 @@
 window.onload = init
 
 function init() {
-    function czyJestSamogloska(char) {
+    function czyJestSamogloska() {
         var samogloski = ["a", "e", "y", "i", "o", "ą", "ę", "u", "ó"]
         var char = prompt("Podaj literę: ")
-        if(!char || !char.match(/^[a-z]+$/i)) {
+        var character = char.charAt(0)
+        //sprawdza czy character jest czymś innym niż litera
+        if(!character || !character.match(/^[a-z]+$/i)) {
             alert("Wpisałeś coś innego niż litera.")
             czyJestSamogloska()
         } else {
-            if(samogloski.includes(char)) {
-                alert(char + " jest samogłoską!")
+            if(samogloski.includes(character)) {
+                alert(character + " jest samogłoską!")
             } else {
-                alert(char + " jest spółgłoską!")
+                alert(character + " jest spółgłoską!")
             }
         }
     }
