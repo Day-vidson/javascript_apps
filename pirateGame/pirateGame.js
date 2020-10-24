675e948747347ebaa5e146e0af8dcb8613ff28ea
dawidLaptop <dawifd54@gmail.com>
2020-10-24 13:36:52 +0200

    pictureGame win

--- pirateGame/pirateGame.js
+++ pirateGame/pirateGame.js
@@ -9,5 +9,12 @@ function showCords(eventObj) {
     var map = document.getElementById("coords")
     var x = eventObj.clientX
     var y = eventObj.clientY
+    var win = document.getElementById("win")
     map.innerHTML = "Współrzędne na mapie: " + x + ", " + y
+
+    if(eventObj.clientX > 175 && eventObj.clientX < 210 && eventObj.clientY > 165 && eventObj.clientY    < 200) {
+        win.innerHTML = "🎉Brawo znalazłeś skarb!🎉"
+    } else {
+        win.innerHTML = "Szukaj dalej"
+    }
 }
