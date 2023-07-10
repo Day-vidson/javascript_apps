7abd3c3ddffc9e921102b21caf252490f1c4ceaa
Dawid Chmiel <dawifd54@gmail.com>
2023-07-10 23:29:11 +0200

    obiekt_posts_pytanie_test2

--- test2.js
+++ test2.js
@@ -1,14 +1,32 @@
-var names = [{name: "dawid"}, {name: "czarek"}, {name: "paola"}]
+var posts = {}
 
-function compareName(name1, name2) {
-    if (name1 > name2) {
-        return 1
-    } else if (name1 === name2) {
-        return 0
-    } else {
-        return -1
-    }
+// Mam obiekt posts, w którym będą inne obiekty nazwa od id posta, które będą przechowywały swoją własną liczbę like'ów
+// posts = {
+//     id1 {
+//         noOflikes: 1
+//         noOfDislikes: 2
+//     },
+//     id2 {
+//         oOflikes: 4
+//         noOfDislikes: 3
+//     }
+// }
+    
+
+// i chciałem później wyszukiwać id w całych posts, zeby znalezc wartosci kazdej reakcji dla kazdego posta
+// wyszukiwanie tylko raz dla kazdego posta, jak znajde id2 to od razu czytam wszystkie wartosci i wyswietlam w komponencie
+
+function addPost(id) {
+    myvar = id
+    posts.{myvar} = newItem(20)
 }
 
-names.sort(compareName)
-console.log(names)
+function newItem(noOflikes) {
+    const post = {noOflikes: noOflikes}
+    return post
+}
+
+addPost("id1")
+addPost("id2")
+
+console.log(posts)
